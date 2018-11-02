21c359e9658482ce75d4b1780b64296c4ef8e499
蔡伟杰 <caiwj@chinamobilesz.com>
2018-11-02 10:56:35 +0800

    06-访问权限控制：Java解释器运行过程（见README.md）；构造函数私有化（简单的单例模式）

---
+++ 06-access-control/src/main/java/szu/vander/access/Lunch.java
@@ -0,0 +1,46 @@
+package szu.vander.access;
+
+import static szu.vander.util.Printer.print;
+
+
+/**
+* @author      : Vander
+* @date        : 2018-11-02
+* @description ：  单例模式
+*/
+public class Lunch {
+
+	public static void main(String args[]) {
+		Soup1 soup1 = Soup1.makeSoup();
+		Soup2.makeSoup().f();
+		print(soup1);
+	}
+	
+}
+
+class Soup1 {
+	
+	private Soup1() {}
+	
+	public static Soup1 makeSoup() {
+		return new Soup1();
+	}
+	
+}
+
+
+class Soup2 {
+	
+	private static Soup2 soup2 = new Soup2();
+	
+	private Soup2() {}
+	
+	public static Soup2 makeSoup() {
+		return soup2;
+	}
+	
+	public void f() {
+		print("f()");
+	}
+	
+}
