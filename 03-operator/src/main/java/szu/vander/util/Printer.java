a28feb4ffe8408e3087ced1a563716bef5daadd3
蔡伟杰 <caiwj@chinamobilesz.com>
2018-11-02 09:16:02 +0800

    03-操作符：自制Jar工具包

---
+++ 03-operator/src/main/java/szu/vander/util/Printer.java
@@ -0,0 +1,43 @@
+
+package szu.vander.util;
+/**
+* @author      : Vander
+* @date        : 2018-11-01
+* @description ： 
+*/
+public class Printer {
+	
+	public static void print(String value) {
+		System.out.println(value);
+	}
+	
+	public static void print(boolean value) {
+		System.out.println(value);
+	}
+	
+	public static void print(byte value) {
+		System.out.println(value);
+	}
+	
+	public static void print(char value) {
+		System.out.println(value);
+	}
+	
+	public static void print(int value) {
+		System.out.println(value);
+	}
+	
+	public static void print(float value) {
+		System.out.println(value);
+	}
+
+	public static void print(double value) {
+		System.out.println(value);
+	}
+
+	public static void print(Object value) {
+		System.out.println(value);
+	}
+
+	
+}
