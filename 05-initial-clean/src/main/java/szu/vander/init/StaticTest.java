61c11355bc9886aca180525e243a6a808859dc7d
Vander1991 <398284823@qq.com>
2019-02-28 22:16:03 +0800

    缺陷修复：修复在父工程中05-初始化与清理章节模块名拼写错误的缺陷

--- 05-initial-clean/src/main/java/szu/vander/init/StaticTest.java
+++ 05-initial-clean/src/main/java/szu/vander/init/StaticTest.java
@@ -1,38 +1,39 @@
 package szu.vander.init;
+
 /**
-* @author : Vander Choi
-* @date : 2018-11-05
-* @description :
-*/
+ * @author : Vander Choi
+ * @date : 2018-11-05
+ * @description :
+ */
 public class StaticTest {
 
-	public static void main(String[] args) {
-		Base sub = new Sub();
-		sub = (Base)new Sub();
-	}
+    public static void main(String[] args) {
+        Base sub = new Sub();
+        sub = (Base) new Sub();
+    }
 
 }
 
 class Base {
-	
-	static {
-		System.out.println("static Base");
-	}
 
-	Base() {
-		System.out.println("Base()");
-	}
-	
+    static {
+        System.out.println("static Base");
+    }
+
+    Base() {
+        System.out.println("Base()");
+    }
+
 }
 
 class Sub extends Base {
-	
-	static {
-		System.out.println("static Sub");
-	}
 
-	Sub() {
-		System.out.println("Sub()");
-	}
-	
+    static {
+        System.out.println("static Sub");
+    }
+
+    Sub() {
+        System.out.println("Sub()");
+    }
+
 }
